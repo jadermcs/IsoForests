be391c8f6664dd68a6acc85feef0deeaa829ab91
Jader <jadermcs94@gmail.com>
2017-09-30 19:53:35 -0300

    add scrap

--- Projeto2/scrap/req.py
+++ Projeto2/scrap/req.py
@@ -10,25 +10,29 @@ def main():
 
     disciplinas = {}
 
-    for table in bsobj.find("div", {"class":"body table-responsive"})\
-              .find_all("div", {"class":"table-responsive"}):
-        for tr in table.find_all("tr")[2:]:
+    for table in bsobj.find('div', {'class':'body table-responsive'})\
+              .find_all('div', {'class':'table-responsive'}):
+        for tr in table.find_all('tr')[2:]:
             a = tr.a
             reqlist = []
 
-            reqs = get(mainurl + a["href"]).content
-            bsin = BeautifulSoup(reqs, "lxml")
-            for k in bsin.find("table", {"id":"datatable"})\
-                         .find_all("td")[5].find_all('strong'):
+            reqs = get(mainurl + a['href']).content
+            bsin = BeautifulSoup(reqs, 'lxml')
+            for k in bsin.find('table', {'id':'datatable'})\
+                         .find_all('td')[5].find_all('strong'):
                 kt = k.text.strip()
                 if kt in disciplinas:
                     reqlist.append(kt)
 
-            disciplinas[a["href"].split('=')[-1]] = {
+            disciplinas[a['href'].split('=')[-1]] = {
                 'nome': a.text.strip(),
                 'requisitos': [x for x in reqlist if x != ' ']
             }
 
+    with open('disciplinas.txt', 'w') as fout:
+        for dsc in disciplinas:
+            fout.write(dsc + '-' + ','.join(disciplinas[dsc]['requisitos']) +
+                       '\n')
 
     pprint(disciplinas)
 
