7d5cb038119460f20bf437a750a38ca4e140610b
Jader Martins <jadermcs94@gmail.com>
2017-11-26 17:38:57 -0200

    refactoring

--- Projeto4/include/isof.h
+++ Projeto4/include/isof.h
@@ -1,19 +1,5 @@
-/** @file dirty.inl
- *  @brief File of libraries.
- *
- *  This file includes all general libraries
- *  of C++ as well as the signature of each funcion of the program.
- *  Also, all global variables are kept here.
- *
- *  @author Dayanne Fernandes da Cunha 13/0107191
- *  @author Renato Avellar Nobre 15/0146698
- *  @bug No bug known.
- */
-
-#ifndef HEADERS_INL_INCLUDED
-#define HEADERS_INL_INCLUDED
-
-/* -- Basic libraries for the better functioning of the program -- */
+#ifndef ISOFOREST
+#define ISOFOREST
 
 #include <iostream>
 #include <fstream>
@@ -21,15 +7,13 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iomanip>
 #include <map>
 #include <cmath>
 
 using namespace std;
 
-/* ----------- Definition of all functions of the program ----------- */
-
-/* -- Functions to show data on command terminal -- */
 void clear();
 void deputySearchUI();
 void displayHelpUI();
@@ -50,7 +34,6 @@ void createCommunities();
 void calcAnomalies();
 
 
-/* -- Functions to create and populate the base graph -- */
 int add_cVertex(string c_name, string c_id);
 int add_dVertex(string d_name, string d_state, string d_party);
 void add_rEdge(string r_desc, string r_date, float r_value, int dVertex, int cVertex);
@@ -59,19 +42,15 @@ int nameExistence(int deputyOrCompany, string name);
 int readFile();
 
 
-/* -- Global variables definitions -- */
 typedef struct cVertex{
     string name;
     string id;
 }t_cVertex;
 
 typedef struct rEdge{
-    /* Tipo de trancação e valor */
     string tipoT;
-    /* Vetor de pares data da trancação e valor */
     vector< pair<string, float> > transac;
     cVertex empresa;
-
 }t_rEdge;
 
 typedef struct dVertex{
@@ -84,4 +63,4 @@ typedef struct dVertex{
 pair< vector<dVertex>, vector<cVertex> > GRAPH;
 map<string, float> COM;
 
-#endif // HEADERS_INL_INCLUDED
+#endif
